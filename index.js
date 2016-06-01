7e3ecc899308329e671f583bf644487410cd6263
Duncan Marsh <dwolfm@gmail.com>
2016-05-31 17:19:10 -0700

    fixed tests and added mongo

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 'use strict';
 
-module.exports = function(){
-  return 'hello travis';
+const mongoose = require('mongoose');
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost/wat';
+mongoose.connect(mongoURI);
+
+const slugSchema = mongoose.Schema({
+  name: String
+});
+
+const Slug = mongoose.model('slug', slugSchema);
+
+exports.createSlug  = function(name){
+  return new Slug({name: name});
+};
+
+exports.saveSlug = function(slug, callback){
+  slug.save(callback);
 };
--- test/index-test.js
+++ test/index-test.js
@@ -1,9 +1,31 @@
 'use strict';
-const assert = require('assert');
+process.env.MONGO_URI = 'mongodb://localhost/slugtest';
+
+const expect = require('chai').expect;
 const hello = require('../index');
 
+
 describe('testing index module', function(){
-  it('should return "hello travis"', function(){
-    assert.ok(hello() === 'hello travi');
+  describe('testing method createSlug', function(){
+    it('should return a slug', function(){
+      expect(hello.createSlug('neo').name).to.equal('neo')
+    });
+  });
+
+  describe('testing method saveSlug', function(){
+    var slug;
+    before((done) => {
+      slug = hello.createSlug('neo slug');
+      done()
+    });
+
+    it('should return a slug', (done ) => {
+      hello.saveSlug(slug, function(err, data){
+        if (err) return done(err);
+        expect(data.name).to.equal('neo slug');
+        done();
+      });
+    });
   });
 });
+
